50f471469da692888fd3574ec91258aa513bd6b8
Cypress <kad2321@yahoo.com>
2024-11-13 20:20:14 -0500

    3.3  Complete

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -31,6 +31,7 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model){
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
@@ -39,7 +40,6 @@ public class EventController {
 
         if(errors.hasErrors()){
             model.addAttribute("title", "Create Event");
-            model.addAttribute("errorMsg", "Bad data!");
             return "events/create";
         }
 
