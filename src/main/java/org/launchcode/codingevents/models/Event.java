2e6401b0b896c943f3f97697cd23ed2e2f49b4ec
Kelly Doherty <kad2321@yahoo.com>
2024-11-17 12:04:24 -0500

    6.1 complete

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,7 +1,10 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,21 +18,18 @@ public class Event extends AbstractEntity {
     @Size(min = 3, max = 50, message= "Name must be between 3 and 50 characters.")
     private String name;
 
-    @Size(max = 500, message = "Description too long.")
-    private String description;
-
-    @NotBlank (message="Email is required.")
-    @Email(message = "Invalid email. Please try again.")
-    private String contactEmail;
-
     @ManyToOne
     @NotNull(message = "Category is required.")
     private EventCategory eventCategory;
 
-    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
+    @NotNull
+    private EventDetails eventDetails;
+
+
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
-        this.description=description;
-        this.contactEmail=contactEmail;
         this.eventCategory=eventCategory;
 
     }
@@ -43,14 +43,6 @@ public class Event extends AbstractEntity {
         this.name = name;
     }
 
-    public @Email String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(@Email String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
     public EventCategory getEventCategory() {
         return eventCategory;
     }
@@ -59,12 +51,12 @@ public class Event extends AbstractEntity {
         this.eventCategory = eventCategory;
     }
 
-    public String getDescription() {
-        return description;
+    public EventDetails getEventDetails() {
+        return eventDetails;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
     }
 
     @Override
